6da32d0c8828d7d8da3a8f5f9beeae4ca640c97d
GSSchneider <35821280+GSSchneider@users.noreply.github.com>
2018-03-12 11:02:40 -0400

    Clone templates, add style.css w/n /public/stylesheets

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 
 // express.static
-app.use(express.static(path.join(__dirname, '/public/views')));
+app.use(express.static(path.join(__dirname, '/public'))); // might be '/public/stylesheets'
 
 // body-parser
 app.use(parser.urlencoded({ extended: true})); // for HTMPL form submits
 app.use(parser.json()); // would be for AJAX reqs
 
 // nunjucks
-const env = nunjucks.configure('/public/views', {noCache: true});
+const env = nunjucks.configure('/views', {noCache: true});
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when res.render works with html files, have it use nunjucks to do so
 
