9d0bb3743dc0a7b976fc96168f6d0575d3663515
Cecilia Avery <cilsong@gmail.com>
2018-03-12 10:35:22 -0400

    setting up

--- app.js
+++ app.js
@@ -7,13 +7,18 @@ const app = express();
 const env = nunjucks.configure('views', {noCache: true});
 const fs = require('fs');
 const path = require('path');
+const morgan = require('morgan');
 
 // have res.render work with html files
 app.set('view engine', 'html');
 // when res.render works with html files, have it use nunjucks to do so
 app.engine('html', nunjucks.render);
+app.use(router);
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.use(router)
 
+if (!module.parent) {
+  app.listen(3000,() => console.log('listening!!'));
+}
 
-let server = app.listen(3000,() => console.log('listening!!'));
